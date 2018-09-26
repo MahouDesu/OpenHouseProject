aec081aeb55cfbdd5940a9f056f88eb54f848d53
Kyle <kylepdockery@gmail.comgit config --global user.name Kyle>
2018-09-26 13:24:47 -0500

    Add project files.

---
+++ OpenHouseProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenHouseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
